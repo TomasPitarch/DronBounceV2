40f73e60138b18275d66247bb35851d6d82e8078
TomyPitarch <tomypitarch@gmail.com>
2025-04-24 00:31:28 -0300

    Rename GameData script

--- Assets/Scripts/Data/GameData_SO.cs
+++ Assets/Scripts/Data/GameData_SO.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/GameDataScriptableObject", order = 1)]
