fd249967d3fcb9a6dc6d116617721de9cde7d300
TomyPitarch <tomypitarch@gmail.com>
2025-04-24 00:30:11 -0300

    Add network, screens and context installers

---
+++ Assets/Scripts/Installers/ContextInstaller.cs
@@ -0,0 +1,17 @@
+
+using UnityEngine;
+using Zenject;
+
+public class ContextInstaller : MonoInstaller<ContextInstaller>
+{
+   [SerializeField]
+   private Navigator navigator;
+   [SerializeField]
+   private GameData_SO gameData;
+   
+   public override void InstallBindings()
+   {
+       Container.BindInstance(navigator).AsSingle();
+       Container.BindInstance(gameData).AsSingle();
+   }
+}
---
+++ Assets/Scripts/Installers/NetworkInstaller.cs
@@ -0,0 +1,14 @@
+using System;
+using Photon.Pun;
+using UnityEngine;
+using Zenject;
+
+public class NetworkInstaller : MonoInstaller<NetworkInstaller>
+{
+    public override void InstallBindings()
+    {
+        Container.Bind<INetworkService>().To<PhotonNetworkService>().AsSingle();
+        Container.Bind<ILoginService>().To<PhotonLoginService>().AsSingle();
+    }
+}
+
