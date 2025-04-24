c74f1f4ad3703a7f9c80dd031a97c4203a06cd27
TomyPitarch <tomypitarch@gmail.com>
2025-04-24 00:21:26 -0300

    Replace old LoginView implementation with new MVP Login and LoginService

---
+++ Assets/Scripts/Login/Presenter/LoginPresenter.cs
@@ -0,0 +1,49 @@
+using Cysharp.Threading.Tasks;
+using Photon.Pun;
+using Zenject;
+
+public class LoginPresenter
+{
+    private LoginView _loginView;
+    
+    private ILoginService _loginService;
+    private Navigator _navigator;
+
+    public LoginPresenter(LoginView loginView,ILoginService loginService, Navigator navigator)
+    {
+        _loginView = loginView;
+        _loginService = loginService;
+        _navigator = navigator;
+
+
+        _loginView.OnConnectButtonClicked += ConnectToRoom;
+        _loginView.DisableConnectButton();
+    }
+  
+    private void ConnectToRoom()
+    {
+        _loginView.DisableConnectButton();
+        PhotonNetwork.NickName = _loginView.GetNickName();
+        _loginService.ConnectToDefaultRoom().ContinueWith(OnLoginResponse);
+    }
+    private void OnLoginResponse(LoginResponse response)
+    {
+        _loginView.DisableConnectButton();  
+        switch (response.Status)
+        {
+            case LoginStatus.Success:
+                LoginSuccess();
+                break;
+            case LoginStatus.InvalidNickName:
+                break;
+            case LoginStatus.Error:
+                break;
+        }
+    }
+    private void LoginSuccess()
+    {
+        //TODO:Move out of here, implement a non generic nickname and
+        // use the navigator instead of a change of scene
+        _navigator.OpenScreen("Teampiker");
+    }
+}
