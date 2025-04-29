0a89c1321ced16c86e2202b5b01586c3b72d781f
TomyPitarch <tomypitarch@gmail.com>
2025-04-29 06:37:13 -0300

    Launch as "forget" a non awaited asynchronous call

--- Assets/Scripts/Initial Connection/Presenter/InitialPresenter.cs
+++ Assets/Scripts/Initial Connection/Presenter/InitialPresenter.cs
@@ -24,7 +24,7 @@ public class InitialPresenter
 
     private void HandleConnectButtonClicked()
     {
-        TryConnect();
+        TryConnect().Forget();
     }
     private async UniTask TryConnect()
     {
