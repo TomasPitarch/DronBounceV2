55111c78850facc8fbced54fb64202a16b645dad
TomyPitarch <tomypitarch@gmail.com>
2025-05-11 19:16:58 -0300

    Add scene manager service interface and implementation

---
+++ Assets/Scripts/Scenes/Service/Implementation/PhotonSceneManagerService.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Zenject;
+
+public class PhotonSceneManagerService :MonoBehaviourPun,ISceneManagerService
+{
+    private UniTask _loadSceneCompletionSource;
+    private IProgress<float> _progress;
+
+    private readonly List<int> _idReadyList=new ();
+
+    [Inject]
+    public void Initialize()
+    {
+        
+    }
+
+    #region ISceneManagerService
+    public void LoadSceneAsyncAllClients(string sceneName)
+    {
+        photonView.RPC("LoadSceneAsyncRPC",RpcTarget.All,sceneName);
+    }
+
+    public IProgress<float> GetSceneProgress()
+    {
+        return _progress;
+    }
+    #endregion
+
+    #region RPC
+
+    [PunRPC]
+    private async void LoadSceneAsyncRPC(string sceneName)
+    {
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneName);
+        //asyncOperation.allowSceneActivation = false;
+        SceneManager.LoadSceneAsync(sceneName).ToUniTask(_progress);
+        _loadSceneCompletionSource= asyncOperation.ToUniTask(_progress);
+
+        await _loadSceneCompletionSource.ContinueWith(ClientLoadSceneComplete);
+    }
+    [PunRPC]
+    private void ClientSceneReadyRPC(int playerId)
+    {
+        _idReadyList.Add(playerId);
+        CheckAllPlayerLoadSceneStatus();
+    }
+    #endregion
+    private void ClientLoadSceneComplete()
+    {
+        photonView.RPC("ClientSceneReadyRPC",RpcTarget.MasterClient,PhotonNetwork.LocalPlayer.ActorNumber);
+    }
+    private void CheckAllPlayerLoadSceneStatus()
+    {
+        Debug.Log("A player end with the charge.");
+        if (_idReadyList.Count is 2)
+        {
+            Debug.Log("All players ready.");
+        }
+    }
+   
+}
