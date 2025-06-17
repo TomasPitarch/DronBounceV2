35e1a1f78d5126356edeb6761c34f275feda65c3
TomyPitarch <tomypitarch@gmail.com>
2025-06-17 06:03:05 -0300

    Remove login service from network installer

--- Assets/Scripts/Installers/NetworkInstaller.cs
+++ Assets/Scripts/Installers/NetworkInstaller.cs
@@ -5,6 +5,5 @@ public class NetworkInstaller : MonoInstaller<NetworkInstaller>
    public override void InstallBindings()
     {
         Container.Bind<INetworkService>().To<PhotonNetworkService>().AsSingle();
-        Container.Bind<ILoginService>().To<PhotonLoginService>().AsSingle();
     }
 }
