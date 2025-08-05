304a55d51826122b11407fcb4cc089549bcb658c
TomyPitarch <tomypitarch@gmail.com>
2025-08-05 06:59:55 -0300

    Update score and game manager and remove old files

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,223 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System;
+using System.Linq;
+using Zenject;
+
+public class GameManager : MonoBehaviourPunCallbacks,IObservable<GameState>
+{
+    private readonly List<IObserver<GameState>> _observers = new ();
+   
+    private BallManager _ballManager;
+
+    [SerializeField]
+    private ScoreManager myScoreManager;
+
+    [SerializeField]
+    private FreezeCounter freezeCounter;
+    
+    [SerializeField]
+    private PlayerSpawner playerSpawner;
+
+    [SerializeField]
+    private int maxPlayerAllowed;
+    
+
+    private Dictionary<PlayerOrder,Player> _playersOrders;
+    private List<Player> _playersInGame=new();
+    private IRoomService _roomService;
+    private HudPresenter _hudPresenter;
+    
+    [Inject]
+    public void Initialize(IRoomService roomService,BallManager ballManager, HudPresenter hudPresenter)
+    {
+        _roomService = roomService;
+        _ballManager = ballManager;
+        _hudPresenter = hudPresenter;
+    }
+    
+    private void Awake()
+    {
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            return;
+        }
+        
+        _playersOrders = new ();
+        OnPlayerEnteredRoom(PhotonNetwork.LocalPlayer);
+        Subscribe(_ballManager);
+
+    }
+    
+    private void Start()
+    {
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            return;
+        }
+        myScoreManager.OnLose += PlayerLose;
+        StartGame();
+    }
+  
+    #region RPC
+
+    [PunRPC]
+    private void SetLoserClientRPC()
+    {
+        _hudPresenter.ShowLose();
+    }
+
+    [PunRPC]
+    private void SetWinnerClientRPC()
+    {
+        _hudPresenter.ShowWin();
+    }
+    [PunRPC]
+    private void SetGameModeRPC()
+    {
+        SetGameMode();
+    }
+
+    [PunRPC]
+    private void SpawnPlayerRPC(PlayerOrder playerOrder)
+    {
+        playerSpawner.SpawnPlayer(playerOrder);
+    }
+    [PunRPC]
+    private void SetPlayerInHudRPC(PlayerOrder playerOrder,Player player)
+    {
+        _hudPresenter.SetPlayerInHud(playerOrder,player);
+    }
+
+    #endregion
+
+    private void SetGameMode()
+    {
+        switch (_roomService.GetTypeOfRoom())
+        {
+            case TypeOfRoom.PvP:
+                _hudPresenter.SetGameMode(TypeOfRoom.PvP);
+                myScoreManager.SetPvpMap();
+                playerSpawner.SetTypeOfRoom(TypeOfRoom.PvP);
+                break;
+            case TypeOfRoom.AllVsAll:
+                _hudPresenter.SetGameMode(TypeOfRoom.AllVsAll);
+                playerSpawner.SetTypeOfRoom(TypeOfRoom.AllVsAll);
+                myScoreManager.SetAllvsAllMap();
+                break;
+            default:
+                Debug.LogError("Unknown room type");
+                break;
+        }
+    }
+    private void PlayerLose(PlayerOrder order)
+    {
+        myScoreManager.EndTriggerFunction(order);
+        _playersInGame.Remove(_playersOrders[order]);
+        photonView.RPC(nameof(SetLoserClientRPC),_playersOrders[order]);
+        
+        WinnerCheck();
+    }
+    private void WinnerCheck()
+    {
+        if (_playersInGame.Count==1)
+        {
+            Player winnerPlayer = _playersInGame.First();
+            photonView.RPC(nameof(SetWinnerClientRPC), winnerPlayer);
+            EndGame();
+        }
+    }
+
+    private async void StartGame()
+    {
+        try
+        {
+            photonView.RPC(nameof(SetGameModeRPC), RpcTarget.All);
+            RegisterPlayers();
+            NotifyGameStart();
+            InitScores();
+            await freezeCounter.StartCounter(5);
+            //TODO:subscribe score manager to listen the start notify
+           
+        }
+        catch (Exception e)
+        {
+            throw e; // TODO handle exception
+        }
+    }
+
+    private void InitScores()
+    {
+        foreach (var valuePair in _playersOrders)
+        {
+            myScoreManager.InitScores(valuePair.Key);
+        }
+    }
+
+    private void RegisterPlayers()
+    {
+        int i = 0;
+        foreach (Player player in PhotonNetwork.PlayerList)
+        {
+            _playersOrders.Add((PlayerOrder)i, player);
+            _playersInGame.Add(player);
+            photonView.RPC(nameof(SetPlayerInHudRPC), RpcTarget.All, (PlayerOrder)i, player);
+            photonView.RPC(nameof(SpawnPlayerRPC), player, (PlayerOrder)i);
+            i++;
+        }
+    }
+
+    private void EndGame()
+    {
+        NotifyGameEnded();
+    }
+    
+    private void NotifyGameEnded()
+    {
+        foreach (IObserver<GameState> observer in _observers)
+        {
+            observer.OnNext(GameState.Ended); 
+        }
+    }
+    
+    private void NotifyGameStart()
+    {
+        foreach (IObserver<GameState> observer in _observers)
+        {
+            observer.OnNext(GameState.Started); 
+        }
+    }
+    
+    private class Unsubscriber<T> : IDisposable
+    {
+        private List<T> _observers;
+        private T _observer;
+
+        public Unsubscriber(List<T> observers, T observer)
+        {
+            this._observers = observers;
+            this._observer = observer;
+        }
+
+        public void Dispose()
+        {
+            if (_observer != null && _observers.Contains(_observer))
+            {
+                _observers.Remove(_observer);
+            }
+        }
+    }
+
+    public IDisposable Subscribe(IObserver<GameState> observer)
+    {
+        _observers.Add(observer);
+        return new Unsubscriber<IObserver<GameState>>(_observers,observer);
+
+    }
+}
+
+
+
+
---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Photon.Pun;
+
+
+
+public class ScoreManager : MonoBehaviourPun
+{
+    //Events//
+    public event Action<PlayerOrder, int> OnScoreGoal ;
+    public event Action<PlayerOrder> OnLose ;
+
+    [SerializeField]
+    private List<ScoreTrigger> triggerList;
+    
+    private Dictionary<PlayerOrder,int> _scoresBinds = new();
+
+    [SerializeField]
+    private GameData_SO GameData;
+    
+    private int _defaultGoal;
+   
+    private void Start()
+    {   
+        TriggerListSubscription();
+        _defaultGoal = GameData.ScoreToLose;
+    }
+    
+    public void InitScores(PlayerOrder order)
+    {
+        _scoresBinds.Add(order,_defaultGoal);
+    }
+    
+    private void TriggerListSubscription()
+    {
+        foreach (ScoreTrigger trigger in triggerList)
+        {
+            trigger.OnScoreTrigger += ScoreGoal;
+        }
+    }
+
+    public void EndTriggerFunction(PlayerOrder order)
+    {
+        photonView.RPC(nameof(EndTriggerRPC), RpcTarget.All, order);
+    }
+   
+    private void ScoreGoal(PlayerOrder playerOrder)
+    {
+        int newScore = _scoresBinds[playerOrder] -1;
+
+        if(newScore<=0)
+        {
+            OnLose?.Invoke(playerOrder);
+        }
+       
+        _scoresBinds[playerOrder] = newScore;
+
+        photonView.RPC(nameof(LightsOnScoreRPC),RpcTarget.All,playerOrder);
+        photonView.RPC(nameof(ScoreGoalRPC), RpcTarget.All, playerOrder, newScore);
+        
+    }
+
+    #region RPC
+
+    [PunRPC]
+    private void ScoreGoalRPC(PlayerOrder playerOrder, int newScore)
+    {
+        OnScoreGoal?.Invoke(playerOrder, newScore);
+    }
+    [PunRPC]
+    public void EndTriggerRPC(PlayerOrder order)
+    {
+        triggerList[(int)order].ChangeTriggerToWall();
+    }
+
+    [PunRPC]
+    public void LightsOnScoreRPC(PlayerOrder order)
+    {
+        triggerList[(int)order].LigthOnScore();
+    }
+   
+
+    #endregion
+
+    public void SetPvpMap()
+    {
+        triggerList[1].ChangeTriggerToWall();
+        triggerList[3].ChangeTriggerToWall();
+        triggerList[2].PlayerOrder= PlayerOrder.Player2;
+    }
+
+    public void SetAllvsAllMap()
+    {
+        
+    }
+    
+    public int GetGoal()
+    {
+        return _defaultGoal;
+    }
+    
+}
