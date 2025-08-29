75dcbdeac1519f6aa84001ea9e0494685918dc6e
TomyPitarch <tomypitarch@gmail.com>
2025-08-29 13:04:00 -0300

    Login presenter implements popup logic for invalid nickname

--- Assets/Scripts/Login/Presenter/LoginPresenter.cs
+++ Assets/Scripts/Login/Presenter/LoginPresenter.cs
@@ -7,16 +7,26 @@ public class LoginPresenter
     private readonly LoginView _loginView;
     private readonly Navigator _navigator;
     private readonly IRoomService _roomService;
+    private readonly IPopUpManager _popUpManager;
 
-    public LoginPresenter(LoginView loginView, Navigator navigator,IRoomService roomService)
+    private readonly PopUpButtonModel _nameErrorPopUpModel;
+        
+
+
+    public LoginPresenter(LoginView loginView, Navigator navigator,IRoomService roomService, IPopUpManager popUpManager)
     {
         _loginView = loginView;
         _navigator = navigator;
         _roomService = roomService;
+        _popUpManager = popUpManager;
 
 
         _loginView.OnConnectButtonClicked += ConnectHandler;
         _loginView.DisableConnectButton();
+        
+         _nameErrorPopUpModel = new PopUpButtonModel("Error : Name already exists, please choose another one.", "Ok");
+         _loginView.SetNickName(_roomService.GetLastNickName());
+        
     }
   
     private void ConnectHandler()
@@ -33,6 +43,7 @@ public class LoginPresenter
             _navigator.OpenScreen("Room");
             break;
             case AuthenticateResponse.NameAlreadyExists:
+                _popUpManager.ShowPopUp(_nameErrorPopUpModel);
                 _loginView.EnableConnectButton();
                 break;
         }
